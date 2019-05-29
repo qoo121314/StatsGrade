f54afc2c69b705403a00a19e269cee78cf2e2ab7
qoo121314@gmail.com <qoo121314@gmail.com>
2019-05-29 20:40:35 +0800

    load & CSV tested

--- application.py
+++ application.py
@@ -6,12 +6,11 @@ import plotly.graph_objs as go
 import pandas as pd
 import numpy as np
 
-
 def fetch_group_name(data,student,Score1,Score2,new):
-    df['RN']=np.zeros(100)
+    data[new]=np.zeros(len(data.index))
     for name, group in data.groupby([Score1,Score2]):
         data.loc[data[data[Score1]==name[0]][data[Score2]==name[1]][new].index,new]=str(" ".join(group[student].values))
-    return data
+    return data[[student,data.columns[1],new]]
 
 def bubble_dict(dictionary , dataframe):
     keys=list(dictionary.keys())
@@ -26,12 +25,10 @@ def bubble_dict(dictionary , dataframe):
 
 df = pd.read_csv('grade1.csv')
 
-option_dict ={ i:i for i in df.columns[2:]}
+option_dict ={  i:i for i in df.columns[2:]}
 bubble_dict= bubble_dict(option_dict,df)
 
-
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets,
                 meta_tags=[
                     {'name': "viewport",
@@ -39,8 +36,6 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets,
                     'initial-scale': 1}
                 ])
 application = app.server
-
-
 
 app.layout=html.Div([
     
@@ -71,8 +66,8 @@ app.layout=html.Div([
     html.Div([
     html.Pre(id='hover-data', style={'paddingTop':35})
     ], style={'width':'30%'})
-
-],style={'padding':10})
+],style={'padding':10}
+)
 
 @app.callback(
     Output('output-graph', 'figure'),
@@ -82,34 +77,33 @@ app.layout=html.Div([
 def update_output_div(input_season,graph_type):
     if graph_type == '盒鬚圖':
         return {
-        'data':[go.Box(y=df[input_season], name= region ) for region in df['班級'].unique() ],
+        'data':[go.Box(y=df[input_season], name= region ) for region in df[df.columns[1]].unique() ],
 
         'layout': go.Layout (title=input_season+"   "+graph_type)}
     elif graph_type == '氣泡圖':
             return {
         'data':[go.Scatter(
-            x=df[df['班級']==region][bubble_dict[input_season]],
-            y=df[df['班級']==region][input_season], 
+            x=df[df[df.columns[1]]==region][bubble_dict[input_season]],
+            y=df[df[df.columns[1]]==region][input_season], 
             name= region,
             mode='markers',
-            text= fetch_group_name(df,'姓名',input_season,bubble_dict[input_season],'RN')[df['班級']==region]['RN']#待釐清,
+            text= fetch_group_name(df,df.columns[0],input_season,bubble_dict[input_season],'RN')[df[df.columns[1]]==region]['RN']
             ,opacity=0.5,
             )
-            for region in df['班級'].unique() ],
+            for region in df[df.columns[1]].unique() ],
 
         'layout': go.Layout (title=input_season+"   "+graph_type,
                             xaxis=dict(title=bubble_dict[input_season]+'成績'),
                             yaxis=dict(title=input_season+'成績'),
                                 hovermode='closest')}
     elif graph_type == '成績表':
-        return {'data':[go.Table(header=dict(values=['姓名','班級',input_season]),
+        return {'data':[go.Table(header=dict(values=[df.columns[0],df.columns[1],input_season]),
                         cells=dict(values=[
-                            df[['姓名','班級',input_season]].dropna().sort_values(by=[input_season],ascending=False)['姓名'],
-                            df[['姓名','班級',input_season]].dropna().sort_values(by=[input_season],ascending=False)['班級'],
-                            df[['姓名','班級',input_season]].dropna().sort_values(by=[input_season],ascending=False)[input_season]
+                            df[[df.columns[0],df.columns[1],input_season]].dropna().sort_values(by=[input_season],ascending=False)[df.columns[0]],
+                            df[[df.columns[0],df.columns[1],input_season]].dropna().sort_values(by=[input_season],ascending=False)[df.columns[1]],
+                            df[[df.columns[0],df.columns[1],input_season]].dropna().sort_values(by=[input_season],ascending=False)[input_season]
                        ]),)]
-                }
-    
+        }
 
 if __name__ == '__main__':
     application.run
